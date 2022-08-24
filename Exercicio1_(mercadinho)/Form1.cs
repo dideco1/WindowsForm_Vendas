e5bb69d58c124f50d4342f1b635e629fa98c5eb5
dideco1 <diegobaro.si@outlook.com>
2022-08-24 10:22:49 -0300

    Add comentários

--- Exercicio1_(mercadinho)/Form1.cs
+++ Exercicio1_(mercadinho)/Form1.cs
@@ -12,10 +12,10 @@ namespace Exercicio1__mercadinho_
 {
     public partial class Louja : Form
     {
-        int venda = 1;
-        int itens_grade = 0;
-        double total = 0;
-        double total_temp = 0;
+        int venda = 1; // 'id' da venda
+        int itens_grade = 0; // itens 'cadastrados' na venda
+        double total = 0; // total de todas as vendas
+        double total_temp = 0; // total temporario para caso a venda atual seja cancelada para voltar ao valor anterior
         public Louja()
         {
             InitializeComponent();
@@ -28,6 +28,7 @@ namespace Exercicio1__mercadinho_
 
         private void btn_nova_venda_Click(object sender, EventArgs e)
         {
+            // cria uma nova venda zerando tudo menos o total, que será armazenado até o programa ser fechado
             venda += 1;
             txt_venda.Text = venda.ToString();
             txt_vlrunit.Clear();
@@ -37,17 +38,19 @@ namespace Exercicio1__mercadinho_
             dgv.Rows.Clear();
             lbl_itens_grade.Text = "";
 
-            total_temp = total;
+            total_temp = total; // armazena o total temporário para caso a próxima venda seja cancelada voltar ao valor anterior
         }
 
         private void btn_inserir_Click(object sender, EventArgs e)
         {
+            // SE algum dos valores forem vazios/nulos dará um erro para o usuário
             if (txt_desc.Text == null || txt_qntd.Text == null || txt_vlrunit.Text == null ||
                 txt_desc.Text == "" || txt_qntd.Text == "" || txt_vlrunit.Text == "")
             {
                 MessageBox.Show("Não preencheu todos campos obrigatórios", "",
                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            // SENÃO cadastrará o produto na venda sem problemas; somando no total etc
             else
             {
                 double vlr_unit = double.Parse(txt_vlrunit.Text);
@@ -69,6 +72,7 @@ namespace Exercicio1__mercadinho_
 
         private void btn_remover_Click(object sender, EventArgs e)
         {
+            // SE não houver nenhum produto cadastrado na venda, dará um erro pois não há produto a ser removido
             if (dgv.RowCount <= 0)
             {
                 MessageBox.Show("Não há vendas para serem excluídas", "",
@@ -76,6 +80,7 @@ namespace Exercicio1__mercadinho_
 
             }
             else
+            // SENÃO remove o produto da venda atual descontando o valor dele do total
             {
                 double vlr_unit;
                 int qntd;
@@ -95,6 +100,7 @@ namespace Exercicio1__mercadinho_
 
         private void btn_alterar_Click(object sender, EventArgs e)
         {
+            // SE algum dos valores que irá ser colocado no produto for vazio/nulo dará um erro
             if (txt_desc.Text == null || txt_qntd.Text == null || txt_vlrunit.Text == null ||
                 txt_desc.Text == "" || txt_qntd.Text == "" || txt_vlrunit.Text == "")
             {
@@ -102,6 +108,7 @@ namespace Exercicio1__mercadinho_
                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
+            // SENÃO alterará a célula selecionada pelo usuário de acordo com o que ele colocar nos campos de registro APÓS selecionar algo da linha desejada para a alteração
             else
             {
 
@@ -125,7 +132,9 @@ namespace Exercicio1__mercadinho_
 
         private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            // Conta e expõe ao usuário quantas células da tabela forem selecionadas com o CTRL+click
             txt_qntd_selecionado.Text = dgv.SelectedCells.Count.ToString();
+            // Pega os dados da célula selecionada e joga nos campos de cadastro acima da tabela para o usuário editar
             txt_desc.Text = dgv.CurrentRow.Cells["dgv_desc"].Value.ToString();
             txt_vlrunit.Text = dgv.CurrentRow.Cells["dgv_vlrunit"].Value.ToString();
             txt_qntd.Text = dgv.CurrentRow.Cells["dgv_qntd"].Value.ToString();
@@ -133,6 +142,7 @@ namespace Exercicio1__mercadinho_
 
         private void btn_cancelar_Click(object sender, EventArgs e)
         {
+            // Cancela todos os cadastros da venda atual e volta o valor do total para o da venda anterior através do total temporário já descrito no começo do código
             txt_venda.Text = venda.ToString();
             txt_vlrunit.Clear();
             txt_qntd_selecionado.Clear();
